6aca88ccaf04edaecbc3fde71a0367fe4623357c
07Sarthak <sarthakprajapati02@gmail.com>
2025-09-01 15:03:38 +0530

    Backend Updated

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const { handleSignUp, handleLogin } = require('../controllers/authController');
+const express=require('express');
+const {handleSignUp, handleLogin} = require('../controllers/authController');
 
-const router = express.Router();
+const router=express.Router();
 
 router.post('/signup', handleSignUp);
 router.post('/login', handleLogin);
 
-module.exports = router;
+module.exports=router;
--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const usersRouter = require('./routes/userRouter');
 const authRouter = require('./routes/authRouter');
-const { auth } = require('firebase-admin');
+const resourcesRouter = require('./routes/resourceRouter');
+const feedbackRouter = require('./routes/feedbackRouter');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/users', usersRouter);
-app.use('/auth', authRouter);
+app.use('/auth',authRouter);
+app.use('/resources', resourcesRouter);
+app.use('/feedback', feedbackRouter);
 
-app.listen(4000, () => console.log('Server running on port 4000')); 
+app.listen(4000, () => console.log('Server running on port 4000'));
